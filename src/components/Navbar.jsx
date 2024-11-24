bae26d7e0b1cc697294be70ee64b9d1e2f773ed0
olusegun <olusegunbamgbelu755@gmail.com>
2024-11-25 00:55:15 +0100

    implement mobile nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import { FiAlignJustify } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+
 export default function Navbar() {
+    const [open, setOpen] = useState(false)
+
+    function handleVisibility() {
+        setOpen(!open)
+    }
+
     return(
-        <nav className="p-6 md:p-8 flex justify-between items-center text-white md:w-full md:max-w-[1280px] md:mx-auto">
+        <div className="p-6 md:p-8 flex justify-between items-center text-white md:w-full md:max-w-[1280px] md:mx-auto relative">
             <h1>
                 Xcode
             </h1>
@@ -13,7 +22,12 @@ export default function Navbar() {
                     <Link to='/safetyTips'>Safety Tips</Link>
                 </ul>
             </nav>
-            <FiAlignJustify className="w-8 h-8 md:hidden"/>
-        </nav>
+            { open ? <AiOutlineClose className="h-6 w-6 md:hidden" onClick={handleVisibility}/> : <FiAlignJustify className="w-6 h-6 md:hidden" onClick={handleVisibility}/>}
+            <div className={`flex flex-col absolute md:hidden top-14 right-0 items-center bg-[#967ced] text-white z-10 w-3/5 max-w-[285px] h-[450px] gap-6 pt-5 rounded-md ${open ? 'opacity-100 scale-100 transform transition-all duration-200 ease-out' : 'opacity-0 pointer-events-none'}`}>
+                <Link to='/' className="text-xl font-semibold">Report Now</Link>
+                <Link to='/resources' className="text-xl font-semibold">Resources</Link>
+                <Link to='/safetyTips' className="text-xl font-semibold">Safety Tips</Link>
+            </div>
+        </div>
     );
 }
