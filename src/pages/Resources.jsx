e70d9e6bbc49e6a26e02b5dd672cbe945af6138b
Hilda <139736274+HildaPosada@users.noreply.github.com>
2024-11-27 07:32:18 +0000

    add text to resources

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -4,7 +4,7 @@ export default function Resources() {
     return (
         <div className="bg-gradient-to-b from-custom-blue to-custom-purple h-screen font-arimo">
             <Navbar />
-            <section className="flex justify-center items-center h-[500px] md:h-[550px] mt-12 w-full">
+            <section className="flex justify-center items-center h-[500px] md:h-[550px] mt-8 w-full">
                 <div className="sm:[95%] w-4/5 max-w-[600px] bg-white px-6 pt-6 pb-8 rounded-lg h-full shadow-custom">
                     <h2 className="text-xl md:text-3xl font-bold text-center">Resources</h2>
                     <ul className="list-disc mt-6 space-y-2 pl-5">
