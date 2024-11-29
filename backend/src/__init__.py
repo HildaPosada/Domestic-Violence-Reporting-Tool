fa67f06101cc2d099b145b1b0b11d233125c7d57
Tope Taiwo <topzyray@yahoo.com>
2024-11-29 12:19:27 +0100

    fix db connection error

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -3,17 +3,18 @@ from contextlib import asynccontextmanager
 from src.db.main import init_db
 
 @asynccontextmanager
-async def life_span(app:FastAPI):
-
+async def life_span(app: FastAPI):
     print("Server is starting")
     await init_db()
     yield
     print("Server Stopped")
 
 app = FastAPI(
-    lifespan = life_span
+    title="Domestic Violence Reporting Tool API",
+    description="This is a backend for the Domestic Violence Reporting Tool project",
+    lifespan=life_span
 )
 
 @app.get("/")
-def index():
+async def index():
     return {"message": "hello world"}
--- backend/src/db/main.py
+++ backend/src/db/main.py
@@ -1,19 +1,20 @@
 from sqlmodel import create_engine, text
-from src.config import db_url
 from sqlalchemy.ext.asyncio import AsyncEngine
+from src.config import db_url
 
 
 async_engine = AsyncEngine(
     create_engine(
-    url = db_url,
-    echo = True
-)
+        url=db_url,
+        echo=True
+    )
 )
 
 async def init_db():
     async with async_engine.begin() as conn:
         statement = text("SELECT 'hello world';")
+
         print(statement)
         result = await conn.execute(statement)
-        print()
+
         print(result.all())
