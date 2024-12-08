fac066c05702ef483c0c1baf1a610570cfcd41ef
Tope Taiwo <topzyray@yahoo.com>
2024-12-08 22:02:35 +0100

    backend: fixed CORS issues

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from src.db.main import init_db
 from src.auth.routes import auth_router
@@ -20,6 +21,20 @@ app = FastAPI(
     description="This is a backend for the Domestic Violence Reporting Tool project",
     version=version,
     lifespan=life_span
+)
+
+origins = [
+    "http://localhost:5174/",
+    "http://localhost:5173/",
+    "https://domestic-violence-reporting-tool.vercel.app/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.add_exception_handler(Exception, global_exception_handler)
